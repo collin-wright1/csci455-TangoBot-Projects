132cdea982ecc379c4a360a95ac0f0cc42d591ba
Collin Wright <collinwright19@gmail.com>
2022-03-02 09:21:13 -0700

    updated testable movement code

--- Project_2_Robot_Movement_Code.py
+++ Project_2_Robot_Movement_Code.py
@@ -5,18 +5,18 @@ class TangoBot:
 
     #initializes robot
     def __init__(self):
-        try:
-            self.usb = serial.Serial('/dev/ttyACM0')
-            print(self.usb.name)
-            print(self.usb.baudrate)
-        except:
-            try:
-                self.usb = serial.Serial('/dev/ttyACM0')
-                print(self.usb.name)
-                print(self.usb.baudrate)
-            except:
-                print("No servo serial ports found")
-                sys.exit(0)
+##        try:
+##            self.usb = serial.Serial('/dev/ttyACM0')
+##            print(self.usb.name)
+##            print(self.usb.baudrate)
+##        except:
+##            try:
+##                self.usb = serial.Serial('/dev/ttyACM0')
+##                print(self.usb.name)
+##                print(self.usb.baudrate)
+##            except:
+##                print("No servo serial ports found")
+##                sys.exit(0)
 
         #movement
         target = 5896
@@ -34,35 +34,98 @@ class TangoBot:
 
         #Ports: 0-1 motor controls, 2 waist, 3-4 head, arms after that
 
-    def move(self, key):
-        print(key.keycode)
+    def moveForward(self, key):
+        print("moving forward")
+        self.makeCommand(6700, 0x01)
+        
 
-    def turn(self, key):
-        print(key.keycode)
+    def moveReverse(self, key):
+        print("moving reverse")
+
+    def turnRight(self, key):
+        print('turning right')
+
+    def turnLeft(self, key):
+        print('turning left')
 
     def stop(self, key):
-        print(key.keycode)
+        print("stopping")
 
     def moveWaist(self, key):
-        print(key.keycode)
+        print("swiveling")
+
+    def moveHead(self, key):
+        print("move head")
+
+    def makeCommand(self, target, port):
+        lsb = target &0x7F
+        msb = (target >> 7) &0x7F
+        cmd = chr(0xaa) + chr(0xC) + chr(0x04) + chr(port) + chr(lsb) + chr(msb)
+        print(cmd)
+        #self.execute(cmd)
+
+    def execute(self, cmd):
+        print('Writing')
+        usb.write(usb.encode('utf-8'))
+        print('Reading')
+        
+
+#implements keyboard input
+class KeyController:
+
+    def __init__(self, robo):
+
+        self.robot = robo
+        
+        win = tk.Tk()
+
+        win.bind('<Up>', self.arrows)
+        win.bind('<Left>', self.arrows)
+        win.bind('<Down>', self.arrows)
+        win.bind('<Right>', self.arrows)
+        win.bind('<space>', self.arrows)
+        win.bind('<z>', self.waist)
+        win.bind('<c>', self.waist)
+        win.bind('<w>', self.head)
+        win.bind('<a>', self.head)
+        win.bind('<s>', self.head)
+        win.bind('<d>', self.head)
+        win.mainloop()
+
+    def arrows(self, key):
+        if(key.keycode == 38):
+            self.robot.moveForward(key)
+        elif(key.keycode == 40):
+            self.robot.moveReverse(key)
+        elif(key.keycode == 37):
+            self.robot.turnLeft(key)
+        elif(key.keycode == 39):
+            self.robot.turnRight(key)
+        elif(key.keycode == 32):
+            self.robot.stop(key)
+
+    def waist(self, key):
+        if(key.keycode == 90):
+            self.robot.moveWaist(key)
+        elif(key.keycode == 67):
+            self.robot.moveWaist(key)
+            
+
+    def head(self, key):
+        if(key.keycode == 87):
+            self.robot.moveHead(key)
+        elif(key.keycode == 65):
+            self.robot.moveHead(key)
+        elif(key.keycode == 83):
+            self.robot.moveHead(key)
+        elif(key.keycode == 68):
+            self.robot.moveHead(key)
     
 
-#Keyboard input
-win = tk.Tk()
-keys = keyControl(win)
+def main():
+    robot = TangoBot()
+    kc = KeyController(robot)
 
-win.bind('<Up>', keys.move)
-win.bind('<Left>', keys.turn)
-win.bind('<Down>', keys.move)
-win.bind('<Right>', keys.turn)
-win.bind('<Space>', keys.stop)
-win.bind('<z>', keys.moveWaist)
-win.bind('<c>', keys.moveWaist)
-win.bind('<w>', keys.moveHead)
-win.bind('<a>', keys.moveHead)
-win.bind('<s>', keys.moveHead)
-win.bind('<d>', keys.moveHead)
-win.mainloop()
-keys = KeyControl(win)
+main()
 
 

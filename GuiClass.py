05a5431c4617fab3de885b351dacd345b6dcd152
wyatwrig <49725228+wyatwrig@users.noreply.github.com>
2022-04-11 12:47:11 -0600

    gui project classes

---
+++ GuiClass.py
@@ -0,0 +1,23 @@
+"""
+Used for rendering the GUI and taking user input 
+"""
+
+class Gui:
+
+    def __init__(self):
+        self.timeline = []
+
+    def createComponent(self):
+        pass
+
+    def createTimeline(self):
+        pass
+
+    def addToTimeline(self, component):
+        pass
+
+    def removeFromTimeline(self, component):
+        pass
+
+    def runTimeline(self):
+        pass

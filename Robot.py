f6c35ce143168c88c8b90b06460d0b13b9f9483c
collin-wright1 <69854643+collin-wright1@users.noreply.github.com>
2022-05-03 13:18:15 -0600

    attempt at gif player

--- Robot.py
+++ Robot.py
@@ -7,6 +7,9 @@ import sys, serial, time
 import speech_recognition as sr
 import pyttsx3
 
+from PySide6.QtWidgets import QApplication, QWidget, QLabel
+from PySide6.QtGui import QIcon, QMovie
+
 class Robot:
 
     # TODO: The print statments need to be changed to be output by the speaker and the input needs to be text to speech.
@@ -24,6 +27,11 @@ class Robot:
         time.sleep(1)
         self.bot.moveForward()
         self.bot.stop()
+        
+        app = QApplication([])
+        window = Window()
+        window.show()
+        sys.exit(app.exec())
 
     # prints the selection menu
     def menu(self):
@@ -391,3 +399,15 @@ class TangoBot:
         self.engine.say(inp)
         self.engine.runAndWait()
 
+class Window(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setGeometry(100, 100, 500, 300)
+        self.setWindowTitle("Python Gif Window")
+        self.setWindowIcon(QIcon('icons/qt.png'))
+        
+        label = QLabel(self)
+        movie = Qmovie('adventure.gif')
+        label.setMovie(movie)
+        movie.start()
+       
